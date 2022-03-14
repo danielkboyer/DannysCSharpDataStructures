a9ae221b73f2308da749333efd65a5834e20ab49
Daniel Boyer <danielkboyer@gmail.com>
2022-03-13 23:24:00 -0600

    First Commit

---
+++ DannysCSharpDataStructures/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DannysCSharpDataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
